4d560271eaddce1c4019db29da04a2833f1bf215
aashu10sh <pudasainiashutosh@gmail.com>
2025-06-17 10:59:36 +0545

    add: multicast_delegate

---
+++ multicast_delegate/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MulticastDeligate
+{
+    public delegate void operation(int x, int y);
+    public class Calculation 
+    {
+        public static void Main(String[] args)
+        {
+            operation obj = Calculation.Add;
+            obj += Calculation.Multiply;
+            obj += Calculation.Sub;
+            obj(10,5);
+            Console.ReadLine();
+        }
+        public static void Add(int a, int b ){
+            Console.WriteLine(a+b);
+        }
+
+        public static void  Sub(int a, int b){
+            Console.WriteLine(a-b);
+        }
+        public static void Multiply(int a, int b){
+            Console.WriteLine(a*b);
+        }
+    }
+
+}
