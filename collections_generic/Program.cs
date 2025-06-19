9c877781e2245850dd4f3a1607e97562a10dc316
aashu10sh <pudasainiashutosh@gmail.com>
2025-06-19 11:17:06 +0545

    add: collections_generic

---
+++ collections_generic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+
+namespace GenericImplementation
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add("Hello ");
+            arrayList.Add("World");
+            arrayList.Add(" !");
+
+            Console.WriteLine("Count is : " + arrayList.Count);
+
+            foreach (string msg in arrayList)
+            {
+                Console.Write("   {0}", msg);
+                Console.WriteLine();
+            }
+
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            dict.Add(69, "rashihang");
+            dict.Add(31, "yujon");
+            dict.Add(21, "balti");
+
+            foreach (KeyValuePair<int, string> pair in dict)
+            {
+                Console.WriteLine(pair.Key + " : " + pair.Value);
+            }
+        }
+    }
+
+}
+
---
+++ partial_class/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PartialClassDemo
+{
+    public partial class Products
+    {
+        public void method1()
+        {
+            Console.WriteLine("method 1 called from partial 1 class");
+        }
+        
+    }
+
+    public partial class Products
+    {
+        public void method2()
+        {
+            Console.WriteLine("method 2 called from partial 2 class");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Products prod = new Products();
+            prod.method1();
+            prod.method2();
+            Console.ReadLine();
+        }
+    }
+}
