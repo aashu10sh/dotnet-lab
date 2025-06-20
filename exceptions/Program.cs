b725400468ff08ed74203a94c4b1aea08ae97bd2
aashu10sh <pudasainiashutosh@gmail.com>
2025-06-20 11:09:11 +0545

    add: new stuff

---
+++ exceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExceptionsImplementation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 0;
+            int x;
+
+            try
+            {
+                x = a / b;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Caught a DivideByZeroException: " + ex.Message);
+            }
+
+            try
+            {
+                int[] numbers = { 1, 2, 3 };
+                Console.WriteLine(numbers[5]);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Caught an IndexOutOfRangeException: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("ggs bro");
+            }
+        }
+    }
+}
---
+++ file_handling/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace FileHandeling
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string fileLocation = "./ggs.txt";
+            FileStream fs = null;
+
+            if (File.Exists(fileLocation))
+            {
+                using (StreamWriter sw = new StreamWriter(fileLocation))
+                {
+                    sw.Write("Yujon Blud");
+                }
+
+                using (StreamReader sr = new StreamReader(fileLocation))
+                {
+                    Console.WriteLine(sr.Read());
+                }
+            }
+            else
+            {
+                File.Create(fileLocation);
+            }
+        }
+    }
+
+}
